5d606445ae505b699eb5f95675012821a79c7ab3
msahrana <msahrana@gmail.com>
2024-04-26 22:31:02 +0600

    env file is added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {MongoClient, ServerApiVersion} = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,10 +8,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crgl3kb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("tourismDB").collection("user");
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ping: 1});
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("SIYANA CARVILA server is running");
+  res.send("Tourism Economics server is running");
 });
 
 app.listen(port, () => {
-  console.log(`SIYANA CARVILA server is running on port: ${port}`);
+  console.log(`Tourism Economics server is running on port: ${port}`);
 });
